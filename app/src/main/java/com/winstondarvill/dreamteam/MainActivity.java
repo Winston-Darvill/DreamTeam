bb4f2b72dc9f94161b6cd98488c130f0edaaba30
Winston-Darvill <winston.p.darvill@gmail.com>
2015-05-23 03:12:25 +1200

    Added a few items and made a VERY simple addition calculator.

--- app/src/main/java/com/winstondarvill/dreamteam/MainActivity.java
+++ app/src/main/java/com/winstondarvill/dreamteam/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v){
+        EditText e1 = (EditText)findViewById(R.id.first_number);
+        EditText e2 = (EditText)findViewById(R.id.second_number);
+        TextView t1 = (TextView)findViewById(R.id.text_result);
+        int num1 = Integer.parseInt(e1.getText().toString());
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int sum = num1 + num2;
+        t1.setText("Results: " + Integer.toString(sum));
     }
 
     @Override
